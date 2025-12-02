3fce51bb2d7b2cdb2e703599d3fdd518ba2e8101
Shevaunie Senior <shevaunie.senior@mymona.uwi.edu>
2025-12-02 02:25:13 -0500

    implemented both country and city search in php. Just table style left

--- world.js
+++ world.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	const btn  = document.querySelector("#lookup");
+	const btn1 = document.querySelector("#lookup_countries");
+	const btn2 = document.querySelector("#lookup_cities");
 	let url = "world.php?";
 	let resultDiv = document.querySelector("div#result");
 	let txtField = document.querySelector("#country");
 	let atr = txtField.getAttribute("name");
 	
-	let search = function(){
+	let searchCountries = function(){
 		
 		let newUrl = url+atr+"="+txtField.value.trim();
 		console.log("Clicked!: "+ newUrl);
@@ -20,5 +21,25 @@ document.addEventListener("DOMContentLoaded", function(event){
 								 console.log("fetch error");
 								});	
 	}
-	btn.onclick = search;
+	
+		let searchCities = function(){
+		
+		let newUrl = url+atr+"="+txtField.value.trim()+"&lookup=cities";
+		console.log("Clicked!: "+ newUrl);
+		
+		fetch(newUrl)
+			.then(response=>{return response.text()})
+			.then(htmlContent=>{
+					resultDiv.innerHTML =""; 
+					resultDiv.innerHTML = htmlContent;
+				}).catch(error=>{resultDiv.innerHTML ="";
+								 resultDiv.innerHTML ='<p class="error"> Couldnt get data</p>';
+								 console.log("fetch error");
+								});	
+	}
+		
+		
+	btn1.onclick = searchCountries;
+	btn2.onclick = searchCities;
+	
 });
--- world.php
+++ world.php
@@ -4,36 +4,61 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$entry ='';
-if(isset($_GET['country'])){
-	$entry = $_GET['country'];
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+$s_entry ='';
+$c_entry ='';
+
+$stateSet= isset($_GET['country']);
+$citySet = isset($_GET['lookup']);
+
+if($stateSet && !$citySet){
+	$s_entry = $_GET['country'];
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%". $s_entry . "%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	
+	echo '<table>';
+		echo '<thead>';
+			echo '<tr>';
+				echo '<th>Country Name</th>';
+				echo '<th>Continent</th>';
+				echo '<th>Independence Year</th>';
+				echo '<th>Head of State</th>';
+			echo '</tr>';
+		echo '</thead>';
+	echo '<tbody>';
+	foreach ($results as $state){
+		echo '<tr>';
+		echo '<td>'.$state['name'].'</td>';
+		echo '<td>'.$state['continent'].'</td>';
+		echo'<td>'.$state['independence_year'].'</td>';
+		echo'<td>'.$state['head_of_state'].'</td>';
+		echo '</tr>';
+	}
+	echo'</tbody></table>';
+}else if($citySet){
+	$s_entry = $_GET['country'];
+	$c_entry = $_GET['lookup'];
+	$stmt = $conn->query("SELECT * FROM cities JOIN countries ON countries.code = cities.country_code WHERE countries.name LIKE '%". $s_entry . "%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo '<table>';
+		echo '<thead>';
+			echo '<tr>';
+				echo '<th>Name</th>';
+				echo '<th>District</th>';
+				echo '<th>Population</th>';
+			echo '</tr>';
+		echo '</thead>';
+	echo '<tbody>';
+	foreach($results as $city){
+		echo '<tr>';
+		echo '<td>'.$city['name'].'</td>';
+		echo '<td>'.$city['district'].'</td>';
+		echo '<td>'.$city['population'].'</td>';
+		echo '</tr>';
+}
+	echo '</tbody></table>';
 }
 
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%". $entry . "%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-
-<table>
-	<thead>
-		<tr>
-			<th>Country Name</th>
-			<th>Continent</th>
-			<th>Independence Year</th>
-			<th>Head of State</th>
-		</tr>
-	</thead>
-	<tbody>
-<?php foreach ($results as $state): ?>
-	<tr>
-  		<td><?=$state['name'];?></td>
-		<td><?=$state['continent'];?></td>
-		<td><?=$state['independence_year'];?></td>
-		<td><?=$state['head_of_state'];?></td>
-	</tr>
-<?php endforeach; ?>
-	</tbody>
-</table>
